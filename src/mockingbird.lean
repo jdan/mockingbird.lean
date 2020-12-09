f6f7ab00625cfbae718c3beb5759b1203fc31e93
Jordan Scales <scalesjordan@gmail.com>
2020-12-09 14:28:39 -0500

    exists,exact -> use, and bring in * with has_mul

--- src/mockingbird.lean
+++ src/mockingbird.lean
@@ -4,7 +4,8 @@ namespace mockingbird
 
 inductive Bird
 | Ap : Bird -> Bird -> Bird
-open Bird
+
+instance : has_mul Bird := ⟨Bird.Ap⟩  /- A * B = Bird.Ap A B -/
 
 /--
   1. One rumor is that Every bird in the forest is fond of
@@ -16,22 +17,22 @@ theorem all_birds_fond
     For any two birds A and B, there is a bird C such that
     for any bird x, Cx = A(Bx)
   -/
-  (C₁ : ∀ A B, ∃ C, ∀ x, Ap C x = Ap A (Ap B x))
+  (C₁ : ∀ (A B : Bird), ∃ C, ∀ x, C * x = A * (B * x))
 
   /-
     (the mockingbird condition)
     The forest contains a mockingbird M
   -/
-  (C₂ : ∃ M, ∀ x, Ap M x = Ap x x)
+  (C₂ : ∃ (M : Bird), ∀ x, M * x = x * x)
 
-  : ∀ A, ∃ B, B = Ap A B :=
+  : ∀ (A : Bird), ∃ B, B = A * B :=
 begin
   intro A,
   cases C₂ with M Hm,
   cases (C₁ A M) with C Hc,   /- Cx = A(Mx) -/
   have CC := Hc C,            /- CC = A(MC) -/
   rw Hm at CC,                /- CC = A(CC) -/
-  existsi Ap C C,
+  existsi C * C,
   exact CC,
 end
 
@@ -40,16 +41,15 @@ end
   Prove using C₁ and C₂ that at least one bird is egocentric.
 -/
 theorem egocentric_bird_exists
-  (C₁ : ∀ A B, ∃ C, ∀ x, Ap C x = Ap A (Ap B x))
-  (C₂ : ∃ M, ∀ x, Ap M x = Ap x x)
-  : ∃ x, x = Ap x x :=
+  (C₁ : ∀ (A B : Bird), ∃ C, ∀ x, C * x = A * (B * x))
+  (C₂ : ∃ (M : Bird), ∀ x, M * x = x * x)
+  : ∃ (x : Bird), x = x * x :=
 begin
   have C₂' := C₂,
   cases C₂ with M Hm,
   cases all_birds_fond C₁ C₂' M with B Hb,  /- B = MB -/
   rw Hm B at Hb,                            /- B = BB -/
-  existsi B,
-  exact Hb,
+  use [B, Hb],
 end
 
 end mockingbird
